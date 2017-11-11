d62274e9e4676f7463889ddae1e7e915637c97e4
Matheus Soares <m.soares.ms@outlook.com>
2017-11-11 00:59:38 -0400

    testando a classe Grafo

---
+++ main.py
@@ -0,0 +1,12 @@
+from Grafo import Grafo
+
+grafo = Grafo(4)
+
+grafo.inserirAresta(1,2)
+grafo.inserirAresta(1,3)
+grafo.inserirAresta(1,4)
+grafo.inserirAresta(2,4)
+grafo.inserirAresta(3,4)
+grafo.inserirAresta(3,2)
+
+grafo.mostrar
